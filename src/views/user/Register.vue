6283e47de72751f149266173c0640be3e7618a12
NinjaLabs <fraser@ninjalabs.co.uk>
2019-10-05 17:54:38 +0100

    Multiple Fixed and additions   (#1)
    * Fixed gap between head and nav
    
    * Removed login button from nav if on login screen
    
    * Signup and login on respected pages
    
    * Removed unneeded whitespace
    
    * Added register vailidation

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -7,24 +7,34 @@
             </div>
             <div class="form-group">
                 <label for="username">Username</label>
-                <input class="form-control" id="username" placeholder="Username" type="text" v-model="username">
+                <input :class="{ 'is-invalid': submitted && !username }" placeholder="Username" class="form-control" id="username" name="username"
+                       type="text" v-model="username"/>
+                <div class="invalid-feedback" v-show="submitted && !username">Username is required</div>
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input class="form-control" id="email" placeholder="Enter email" type="email" v-model="email">
+                <input :class="{ 'is-invalid': submitted && !email }" placeholder="Email" class="form-control" id="email" name="email"
+                       type="email" v-model="email"/>
+                <div class="invalid-feedback" v-show="submitted && !email">Email is required</div>
                 <small class="form-text text-muted" id="emailHelp">We'll never share your email with anyone
                     else.</small>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input class="form-control" id="password" placeholder="Password" type="password" v-model="password">
+                <input :class="{ 'is-invalid': submitted && !password }" placeholder="Password" class="form-control" id="password" name="password"
+                       type="password" v-model="password"/>
+                <div class="invalid-feedback" v-show="submitted && !password">Password is required</div>
             </div>
             <div class="form-group">
                 <label for="passwordRepeat">Repeat Password</label>
-                <input class="form-control" id="passwordRepeat" placeholder="Password Repeat" type="password"
-                       v-model="passwordRepeat">
+                <input :class="{ 'is-invalid': submitted && !passwordRepeat || password !== passwordRepeat}" placeholder="Password Repeat" class="form-control" id="password-repeat" name="password"
+                       type="password" v-model="passwordRepeat"/>
+                <div class="invalid-feedback" v-show="submitted && !passwordRepeat">Repeated Password is required</div>
+                <div class="invalid-feedback" v-show="submitted && password !== passwordRepeat || password !== passwordRepeat">Passwords arn't the same</div>
             </div>
-            <button class="btn btn-primary" type="submit">Submit</button>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
         </form>
     </div>
 </template>
@@ -40,12 +50,19 @@
                 username: '',
                 email: '',
                 password: '',
+                submitted: false,
                 passwordRepeat: '',
                 errors: []
             }
         },
         methods: {
             registerUser() {
+                this.submitted = true;
+
+                if(!(this.username && this.email && this.password && this.passwordRepeat)) {
+                    return;
+                }
+
                 userService.makeRequestToAPIWithoutAuth('/register', {
                     username: this.username,
                     email: this.email,
