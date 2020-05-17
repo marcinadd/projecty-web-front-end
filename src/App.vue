9c6ed6f826477d69edfb8c4ad72033a47419d33e
Joseph Betts <38386086+JBetts97@users.noreply.github.com>
2020-05-17 15:21:08 +0100

    Footer always shown at bottom of window

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,50 @@
 <template>
-  <div id="app">
-      <TheNav/>
-      <TheHeader title="Hello"></TheHeader>
-      <Notification></Notification>
+<div id="app">
+  <body class="site">
+    <TheNav />
+    <TheHeader title="Hello"></TheHeader>
+    <Notification></Notification>
+    <main class="site-content">
       <Section></Section>
-      <TheFooter/>
-  </div>
+    </main>
+    <TheFooter />
+  </body>
+</div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader";
+import TheFooter from "@/components/TheFooter";
+import TheNav from "@/components/TheNav";
+import Section from "@/components/Section";
+import Notification from "@/components/Notification";
 
-
-    import TheHeader from "@/components/TheHeader";
-    import TheFooter from "@/components/TheFooter";
-    import TheNav from "@/components/TheNav";
-    import Section from "@/components/Section";
-    import Notification from "@/components/Notification";
-
-    export default {
-        name: "app",
-        components: {Notification, Section, TheNav, TheFooter, TheHeader}
-}
+export default {
+  name: "app",
+  components: { Notification, Section, TheNav, TheFooter, TheHeader }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 55px;
+  margin-top: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.site {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+.site-content {
+  flex: 1;
 }
 </style>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,17 +1,17 @@
 <template>
-    <footer class="py-5 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Projecty Web 2019</p>
-        </div>
-    </footer>
+  <footer class="py-5 bg-dark">
+    <p class="m-0 text-center text-white">Copyright &copy; Projecty Web 2019</p>
+  </footer>
 </template>
 
 <script>
-    export default {
-        name: "TheFooter"
-    }
+export default {
+  name: "TheFooter"
+};
 </script>
 
 <style scoped>
-
+footer {
+  bottom: 0;
+}
 </style>
